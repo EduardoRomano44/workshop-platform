aaa20381a238bef26cb8f480b426d6c04a7f467c
Eduardo Romano <eduardo.romano@ua.pt>
2026-02-25 17:42:07 +0000

    revert

--- platform/backend.tf
+++ platform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "workshop-ua-${var.environment}-terraform-state-bucket-version-testing"
+    bucket         = "workshop-ua-terraform-state-bucket-version-testing"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
